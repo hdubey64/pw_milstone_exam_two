2f57d5ca35518859ebbb2fd9c55314cdc2439b44
Vishal Dubey <hdubey64@gmail.com>
2023-08-15 23:47:09 +0530

    Navbar Done

--- projects-1/assets/javascript/index.js
+++ projects-1/assets/javascript/index.js
@@ -1,17 +1,23 @@
 const createBtn = document.getElementById("create");
+const mediaQueryXm = window.matchMedia("(min-width: 350px)");
+const mediaQueryLg = window.matchMedia("(max-width: 1199.98px)");
 
 createBtn.addEventListener("mouseover", () => {
+   if (mediaQueryLg.matches) {
+      return;
+   }
    createBtn.innerHTML = "Click me to write";
    createBtn.style.backgroundColor = "transparent";
    createBtn.style.fontWeight = "bolder";
    createBtn.style.width = "200px";
+   createBtn.style.color = "#2A9D8F";
 });
 
 createBtn.addEventListener("mouseout", () => {
-   setTimeout(() => {
-      createBtn.innerHTML = '<i class="bi bi-plus-lg btnLogo">';
-      createBtn.style.backgroundColor = "#e2c0ff";
-      createBtn.style.width = "100px";
-      createBtn.style.fontWeight = "bolder";
-   }, 100);
+   createBtn.innerHTML = '<i class="bi bi-plus-lg btnLogo"></i>';
+   createBtn.style.backgroundColor = "#2a9d8f";
+   createBtn.style.width = "100px";
+   createBtn.style.fontWeight = "bolder";
+   //    createBtn.style.transition = "bolder";
+   createBtn.style.color = "white";
 });
