ce606b98a81ef33614dded5d65a96062c1faa6cf
Vishal Dubey <hdubey64@gmail.com>
2023-08-20 12:57:53 +0530

    Card botton event listener done

--- projects-1/assets/javascript/index.js
+++ projects-1/assets/javascript/index.js
@@ -1,16 +1,19 @@
 const createBtn = document.getElementById("create");
 const mediaQueryXm = window.matchMedia("(min-width: 350px)");
 const mediaQueryLg = window.matchMedia("(max-width: 1199.98px)");
+const readBtn = document.getElementById("read");
+const stars = document.querySelectorAll(".star");
+const starRating = document.querySelector(".star-rating");
 
 createBtn.addEventListener("mouseover", () => {
    if (mediaQueryLg.matches) {
       return;
    }
    createBtn.innerHTML = "Click me to write";
-   createBtn.style.backgroundColor = "transparent";
+   createBtn.style.backgroundColor = "#2a9d8f";
    createBtn.style.fontWeight = "bolder";
    createBtn.style.width = "200px";
-   createBtn.style.color = "#2A9D8F";
+   createBtn.style.color = "white";
 });
 
 createBtn.addEventListener("mouseout", () => {
@@ -20,4 +23,23 @@ createBtn.addEventListener("mouseout", () => {
    createBtn.style.fontWeight = "bolder";
    //    createBtn.style.transition = "bolder";
    createBtn.style.color = "white";
+});
+
+readBtn.addEventListener("mouseover", () => {
+   if (mediaQueryLg.matches) {
+      return;
+   }
+   readBtn.innerHTML = "Click me to Read this Article";
+   readBtn.style.backgroundColor = "#2A9D8F";
+   readBtn.style.fontWeight = "bolder";
+   readBtn.style.width = "100%";
+   readBtn.style.color = "white";
+});
+
+readBtn.addEventListener("mouseout", () => {
+   readBtn.innerHTML = "Read";
+   readBtn.style.backgroundColor = "#a2ded0";
+   readBtn.style.width = "100%";
+   readBtn.style.fontWeight = "bolder";
+   readBtn.style.color = "#2A9D8F";
 });
